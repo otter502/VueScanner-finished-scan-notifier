9f5cbce360600fe0dd6c6d65bb8a3c79efd75868
otter502 <115671877+otter502@users.noreply.github.com>
2026-05-15 11:48:58 -0700

    added fatal error for debugging

--- HelloWorld.py
+++ HelloWorld.py
@@ -43,40 +43,44 @@ scanStatus = 0
 
 print("started monitoring @ ", time.asctime())
 
-while(app.top_window().exists()):
-    try:
-        currText = app.top_window().StatusBar.children()[0].window_text()
-    except:
-        currText = lastText
-        print("error pls fix! @ ", time.asctime(), "\n", currText, " : ", lastText, " : ", scanStatus)
-    
-    # if last text contained "save" or "scan" and curr text does not?
-
-    # neither = 0
-    # scanning = 1
-    # previewing = 2
-    def isScanningStatus(s):
-        return (1 if (s.lower().startswith("scan") or ("save" in s.lower())) else 0) + (2 if (s.lower().startswith("preview")) else 0)
-
-    if (isScanningStatus(currText) == 0 and scanStatus != 0): # something finished!
-            playsound("./notifSound.mp3")
-            if (scanStatus == 2):
-                print("finished previewing @ ", time.asctime())
-            if (scanStatus == 1):
-                print("finished scanning @ ", time.asctime())
-            scanStatus = 0
-
-
-
-    if (isScanningStatus(currText)!= 0 and scanStatus == 0): # something started!
-        if (isScanningStatus(currText) == 2):
-            print("started previewing @ ", time.asctime())
-        if (isScanningStatus(currText) == 1):
-            print("started scanning @ ", time.asctime())
-        scanStatus = isScanningStatus(currText)
+try:
+    while(app.top_window().exists()):
+        try:
+            currText = app.top_window().StatusBar.children()[0].window_text()
+        except:
+            currText = lastText
+            print("error pls fix! @ ", time.asctime(), "\n", currText, " : ", lastText, " : ", scanStatus)
         
+        # if last text contained "save" or "scan" and curr text does not?
+
+        # neither = 0
+        # scanning = 1
+        # previewing = 2
+        def isScanningStatus(s):
+            return (1 if (s.lower().startswith("scan") or ("save" in s.lower())) else 0) + (2 if (s.lower().startswith("preview")) else 0)
+
+        if (isScanningStatus(currText) == 0 and scanStatus != 0): # something finished!
+                playsound("./notifSound.mp3")
+                if (scanStatus == 2):
+                    print("finished previewing @ ", time.asctime())
+                if (scanStatus == 1):
+                    print("finished scanning @ ", time.asctime())
+                scanStatus = 0
 
 
 
-    lastText = currText
-    time.sleep(1)
+        if (isScanningStatus(currText)!= 0 and scanStatus == 0): # something started!
+            if (isScanningStatus(currText) == 2):
+                print("started previewing @ ", time.asctime())
+            if (isScanningStatus(currText) == 1):
+                print("started scanning @ ", time.asctime())
+            scanStatus = isScanningStatus(currText)
+            
+
+
+
+        lastText = currText
+        time.sleep(1)
+except:
+    print("fatal error pls fix! @ ", time.asctime(), "\n", currText, " : ", lastText, " : ", scanStatus)
+
