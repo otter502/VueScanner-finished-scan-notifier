156abdc6af6f889545648d26387600e925d0a48b
otter502 <115671877+otter502@users.noreply.github.com>
2026-05-18 14:54:05 -0700

    -

--- classnameSearching.py
+++ classnameSearching.py
@@ -7,6 +7,9 @@ w_handle = pywinauto.findwindows.find_windows(class_name_re=r'.+')
 
 for i in w_handle:
     print(i, "\t",end="")
-    pwa_app.connect(handle = i)
-    print(pwa_app.top_window().class_name(), "\t", pwa_app.top_window().window_text())
+    try:
+        pwa_app.connect(handle = i)
+        print(pwa_app.top_window().class_name(), "\t", pwa_app.top_window().window_text())
+    except:
+        print("welp shoot")
 # app = pwa_app.connect(handle = w_handle[0])
