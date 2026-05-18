ce145f9ba1d21e0757fa0442bac678b16057a4da
otter502 <115671877+otter502@users.noreply.github.com>
2026-05-18 14:53:13 -0700

    Okay it's an issue with connecting to the window or finding it

---
+++ classnameSearching.py
@@ -0,0 +1,12 @@
+import pywinauto
+
+# app = Application(backend="uia").connect()
+
+pwa_app = pywinauto.application.Application(backend="uia")
+w_handle = pywinauto.findwindows.find_windows(class_name_re=r'.+')
+
+for i in w_handle:
+    print(i, "\t",end="")
+    pwa_app.connect(handle = i)
+    print(pwa_app.top_window().class_name(), "\t", pwa_app.top_window().window_text())
+# app = pwa_app.connect(handle = w_handle[0])
